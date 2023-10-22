929bda25e44d7541e7f1560ceb7f912cd845ba15
vinod7967 <kotha.vinodkumar11111@gmail.com>
2023-10-23 00:02:19 +0530

    allowed hosts added

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q&50_yo8sn#dzi&!-s$f_xr@l$$rw%jbe5r)4tb+tl5w+7ud92
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
